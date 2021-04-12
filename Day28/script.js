c4609155fe4403b3bb132bd13064d97cfc536c0c
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2021-04-12 08:04:42 +0530

    github profiles

---
+++ Day28/script.js
@@ -0,0 +1,92 @@
+const API_URL = 'http://api.github.com/users/'
+
+const form = document.getElementById('form')
+const main = document.getElementById('main')
+const search = document.getElementById('search')
+
+
+async function getUser(username) {
+    try {
+        const {data} = await axios(API_URL + username)
+
+        createUserCard(data)
+        getRepos(username)
+    } catch (error) {
+        if (error.response.status === 404) {
+            createErrorCard('No profile with this username')
+        }
+    }
+}
+
+async function getRepos(username) {
+    try {
+        const {data} = await axios(API_URL + username + '/repos?sort=created')
+
+        addReposToCard(data)
+    } catch (error) {
+        if (error) {
+            createErrorCard('Problem fetching repos')
+        }
+    }
+}
+
+function createUserCard(user) {
+    const cardHtml = `
+    <div class="card">
+    <div>
+      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+    <div class="user-info">
+      <h2>${user.name}</h2>
+      <p>${user.bio}</p>
+      <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
+
+      <div id="repos"></div>
+    </div>
+  </div>
+    `
+    main.innerHTML = cardHtml
+}
+
+
+function createErrorCard(msg) {
+    const cardHtml = `
+        <div class="card">
+            <h1>${msg}</h1>
+        </div>
+    `
+
+    main.innerHTML = cardHtml
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos')
+
+    repos 
+        .slice(0, 5)
+        .forEach(repo => {
+            const repoEl = document.createElement('a')
+            repoEl.classList.add('repo')
+            repoEl.href = repo.html_url
+            repoEl.target = '_blank'
+            repoEl.innerText = repo.name
+
+            reposEl.appendChild(repoEl)
+        })
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if (user) {
+        getUser(user)
+
+        search.value = ''
+    }
+})
